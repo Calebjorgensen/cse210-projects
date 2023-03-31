6d080a56dd6761c5692152f07727cae853df0066
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-03-31 11:59:36 -0700

    This show no error now

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -6,24 +6,11 @@ class Program
     {
         Console.WriteLine();
 
-            // This address is not real come back and make up a new one. 
-        Address address1 = new Address("Fake address for now");
+        Address address1 = new Address("345 road", "","","");
 
-            // This isnt a real customer fix later.
-        Customer customer1 = new Customer("Fake customer", address1);
+        Customer customer1 = new Customer("Paul", address1);
 
-        List<Product> productsList = new List<Product>();
-
-
-            // this product is fake as well. 
-        Product Pr1 = new Product("Fake item");
-
-        productList1.Add(Pr1);
-
-        Order order1 = new Order(productsList1, customer1);
-
-        order1.DisplayResults();
-
+        List<Product> productsList1 = new List<Product>();
 
 
     }
