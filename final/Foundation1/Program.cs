eae02aa5557e4afc66da3cb2ed56a488dc75843c
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-04-01 11:57:53 -0700

    trying something new

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,22 +6,7 @@ class Program
     {
         Console.WriteLine();
 
-        List<Video> videosList = new List<Video>();
 
-        Video video1 = new Video("How to run new Halo mission");
-
-        Comment video1Commment1 = new Comment("This was great");
-
-        video1.ListComment(video1Commment1);
-
-        videosList.Add(video1);
-
-        foreach(Video video in videosList)
-        {
-            video.DisplayInfo();
-            
-            
-        }
 
 
         
