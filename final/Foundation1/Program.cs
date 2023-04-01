a17802f1d34014e812fa95a697cd2b99e9a1534a
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-04-01 13:21:37 -0700

    I figure it out wasnt filling the classes

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,6 +6,34 @@ class Program
     {
         Console.WriteLine();
 
+        List<Video> videoList = new List<Video>();
+
+        Video video1 = new Video("Creed3","By Caleb",5.4);
+
+        Comment video1Comment1 = new Comment("Caleb","This video is great");
+        Comment video1Comment2 = new Comment("Elle","this is great");
+        Comment video1Comment3 = new Comment("Paul","Why is this in error");
+
+        video1.ListComment(video1Comment1);
+        video1.ListComment(video1Comment2);
+        video1.ListComment(video1Comment3);
+
+        videoList.Add(video1);
+
+        Video video2 = new Video("How to run halo mission","By Elle",6.4);
+        Comment video2Comment1 = new Comment("elle","this is great");
+
+        video2.ListComment(video2Comment1);
+
+        videoList.Add(video2);
+
+        foreach(Video video in videoList)
+        {
+            video.DisplayInfo();
+        
+        }
+
+
 
 
 
