f3c6c10494f837a279fb4f239d686fdc6751c5c9
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-03-31 17:18:16 -0700

    address and lecture not working

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Console.WriteLine("");
+
+        Address address = new Address("832 fake street");
+
+        Lecture lecture = new Lecture("Rocky 3 is better then Cree 3");
+
+        Console.WriteLine("Event 1");
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateStandard());
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateDetailedLecture());
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateShortLecutre());
+
+        Address address1 = new Address("A-1 warehouse");
+        Reception reception = new Reception("Joey and el", address);
     }
 }
