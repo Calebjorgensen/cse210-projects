4458f112ddafc38a3ee68b81b7ee63c163b5aea8
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-03-31 23:51:19 -0700

    updated code

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Console.WriteLine();
+
+        Console.WriteLine("Running");
+        Running running = new Running("31 mar 2023",30,5);
+        Console.WriteLine(running.GetSummary());
+
+
+        Console.WriteLine("Cycling");
+        Bicycles bicycles = new Bicycles("1 April 2023", 45, 10);
+        Console.WriteLine(bicycles.GetSummary());
+
+        Console.WriteLine("Swimming");
+        Swimming swimming = new Swimming("2 April 2023", 20,20);
+        Console.WriteLine(swimming.GetSummary());
     }
 }
