b77829199bffb328eab0c1746a9fc8df7938fe39
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-04-01 17:29:11 -0700

    entry code updated

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,16 @@
+using System;
+public class Entry
+{
+    private string _entry;
+
+    public Entry(string Entry)
+    {
+        _entry = Entry;
+
+    }
+
+    public void DisplayInfo()
+    {
+        
+    }
+}
