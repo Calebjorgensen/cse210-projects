e8b2621351ac883b6d9034ce82ef8846e9c69317
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-04-01 20:38:01 -0700

    last update

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -6,19 +6,26 @@ class Program
     {
         Console.WriteLine();
 
-        Address address1 = new Address("345 road", "","","");
+
+
+        Address address1 = new Address("345 road", "Nowhereville","OR","USA");  // the main address for the code
 
         Customer customer1 = new Customer("Paul", address1);
 
+// The line above is for the first customer
+// The line below is for a list of products that are being sold.
+
         List<Product> productsList1 = new List<Product>();
 
-        Product Pr1 = new Product("Coupling black", "", 20,1);
+        Product Pr1 = new Product("Coupling black", "This is a device that connects to items ", 20,1); //both of these lines are in product class
+        Product Pr2 = new Product("Xbox controller","This is a xbox accessory", 60,1);
         
 
         productsList1.Add(Pr1);
         
 
-        Order order1 = new Order(productsList1, customer1);
+        Order order1 = new Order(productsList1, customer1); // order class. 
+        Order order2 = new Order(productsList1,customer1);
 
         order1.DisplayResults();
 
