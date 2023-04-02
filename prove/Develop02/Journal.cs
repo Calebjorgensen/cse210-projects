dfb813a2dd17136ac72b9eec9dd6cac0fb894ca6
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-04-01 17:26:17 -0700

    working on brnach

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,14 +1,25 @@
 using System;
 
-public class Journal{
+//public class Journal{
 
-    static void Main(string[] args){
+   // static void Main(string[] args){
 
-        Entry record = new Entry();
+     //   Entry record = new Entry();
         
+   // }
+//}
 
+
+public class Journal
+{
+    private string _display;
+    private string _load;
+    private string _save;
+    private string _quit;
+
+    public Journal(string Display, string Load, string Save, string Quit)
+    {
+        _display = Display;
     }
-
-
 
 }
