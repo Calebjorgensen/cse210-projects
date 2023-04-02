2f89c1e8f2f96ce23cd75ee09f2b8d381e13bd3e
Caleb Jorgensen <calebjorgensen@Calebs-Air.lan>
2023-04-01 20:50:24 -0700

    last commit

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,15 +6,22 @@ class Program
     {
         Console.WriteLine();
 
+        // The Exercise class does all the main work. 
+
+// The code is super simple its just a copy over of the lines from each of the classes. 
+
+        // this is the running class
         Console.WriteLine("Running");
         Running running = new Running("31 mar 2023",30,5);
         Console.WriteLine(running.GetSummary());
 
-
+        // This is the cycling class
         Console.WriteLine("Cycling");
         Bicycles bicycles = new Bicycles("1 April 2023", 45, 10);
         Console.WriteLine(bicycles.GetSummary());
 
+
+        // this is the Swimminglaps class
         Console.WriteLine("Swimming");
         Swimming swimming = new Swimming("2 April 2023", 20,20);
         Console.WriteLine(swimming.GetSummary());
